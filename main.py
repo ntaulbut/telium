4ebc51c497dbada8ece78b8c221f6e7f7dbe8524
Nathaniel Taulbut <ntaulbut@gmail.com>
2023-10-27 17:30:30 +0100

    format with black formatter

--- main.py
+++ main.py
@@ -7,11 +7,26 @@ from utils import *
 
 # Constants
 MODULES_FILENAME = "space_modules.json"
-DIRECTIONS = ["north", "northeast", "east", "southeast", "south", "southwest",
-              "west", "northwest"]
-DIRECTION_ALIASES = {"n": "north", "ne": "northeast", "e": "east",
-                     "se": "southeast", "s": "south", "sw": "southwest",
-                     "w": "west", "nw": "northwest"}
+DIRECTIONS = [
+    "north",
+    "northeast",
+    "east",
+    "southeast",
+    "south",
+    "southwest",
+    "west",
+    "northwest",
+]
+DIRECTION_ALIASES = {
+    "n": "north",
+    "ne": "northeast",
+    "e": "east",
+    "se": "southeast",
+    "s": "south",
+    "sw": "southwest",
+    "w": "west",
+    "nw": "northwest",
+}
 # Gameplay constants
 NUM_WORKER_ALIENS = 4
 LOCK_MODULE_ENERGY = 20
@@ -23,11 +38,13 @@ command_helps: List[str] = []
 
 def command(word: str):
     """Register a command."""
+
     def wrap(func: Callable):
         available_commands[word] = func
         if func.__doc__ is not None:
             command_helps.append(func.__doc__)
         return func
+
     return wrap
 
 
@@ -45,7 +62,7 @@ def list_available_commands(_):
     """commands: List the available commands."""
     cprint("You can use these commands:")
     for command_help in command_helps:
-        cprint(" - " + command_help, long_delay=.02)
+        cprint(" - " + command_help, long_delay=0.02)
 
 
 @command("doors")
@@ -63,20 +80,27 @@ class ModuleInterface(object):
     """Allows referencing a module directly instead of by indexing the global
     list of modules. Raises an assertion error if it is attempted to be created
     for a module that does not exist."""
+
     def __init__(self, module_id):
         assert module_id in list(station.modules.keys())
         object.__setattr__(self, "module_id", module_id)
 
     def __getattribute__(self, name):
-        return station.modules[object.__getattribute__(self, "module_id")].__getattribute__(name)
+        return station.modules[
+            object.__getattribute__(self, "module_id")
+        ].__getattribute__(name)
 
     def __setattr__(self, name, value):
-        station.modules[object.__getattribute__(self, "module_id")].__setattr__(name, value)
+        station.modules[object.__getattribute__(self, "module_id")].__setattr__(
+            name, value
+        )
 
     def __eq__(self, other):
         # Two ModuleInterfaces are equal if they point to the same module
         if isinstance(other, self.__class__):
-            return object.__getattribute__(self, "module_id") == object.__getattribute__(other, "module_id")
+            return object.__getattribute__(
+                self, "module_id"
+            ) == object.__getattribute__(other, "module_id")
         # A ModuleInterface is equal to a string if it's the module id it points to
         elif isinstance(other, str):
             return object.__getattribute__(self, "module_id") == other
@@ -87,11 +111,14 @@ class ModuleInterface(object):
         return not self == other
 
 
-class Module():
+class Module:
     """Space station module."""
-    d_enter = Dialogue("You find yourself in the {title}.",
-                       "You step into the {title}.",
-                       "You cautiously enter the {title}.")
+
+    d_enter = Dialogue(
+        "You find yourself in the {title}.",
+        "You step into the {title}.",
+        "You cautiously enter the {title}.",
+    )
 
     def __init__(self, title, description, doors):
         self.title: str = title
@@ -117,11 +144,20 @@ class Module():
         cprint("There is a door on the", end=" ")
         cprint(sent_concat(list(self.doors)), end=" ")
         cprint(f"side{'s'[:len(self.doors)^1]}.")
-    
+
     def random(exclude: List[ModuleInterface] = []) -> ModuleInterface:
         """Return a random module, optionally excluding some of them."""
-        return ModuleInterface(choice(list(filter(lambda mid: not ModuleInterface(mid) in exclude, station.modules.keys()))))
-    
+        return ModuleInterface(
+            choice(
+                list(
+                    filter(
+                        lambda mid: not ModuleInterface(mid) in exclude,
+                        station.modules.keys(),
+                    )
+                )
+            )
+        )
+
     @command("modules")
     def list_modules(_):
         """modules: List the modules in the station."""
@@ -133,6 +169,7 @@ class Module():
 
 class Entity:
     """An entity that has a physical presence in a module."""
+
     def __init__(self, initial_module):
         entities.append(self)
         self.module: ModuleInterface = initial_module
@@ -161,7 +198,10 @@ class Hurtable:
             self._health -= attack
         else:
             self._health = 0
-        logger.log(f"{unique_name(self)} took {attack} damage, now at {self._health} health.", LogLevel.VERBOSE)
+        logger.log(
+            f"{unique_name(self)} took {attack} damage, now at {self._health} health.",
+            LogLevel.VERBOSE,
+        )
         if self._health == 0:
             self.alive = False
             logger.log(f"{unique_name(self)} died.")
@@ -169,6 +209,7 @@ class Hurtable:
 
 class Player(Entity, Hurtable):
     """Player singleton."""
+
     def __init__(self, initial_module):
         Entity.__init__(self, initial_module)
         Hurtable.__init__(self, 100)
@@ -182,18 +223,27 @@ class Player(Entity, Hurtable):
         if introduce:
             self.module.introduce()
         self.module.visited_by_player = True
-        logger.log(f"Other entities in this module: {[unique_name(entity) for entity in list(filter(lambda entity: not isinstance(entity, Player), self.module.entities))]}")
+        logger.log(
+            f"Other entities in this module: {[unique_name(entity) for entity in list(filter(lambda entity: not isinstance(entity, Player), self.module.entities))]}"
+        )
         # Execute observers
-        for entity in list(filter(lambda entity: not isinstance(entity, Player), self.module.entities)):
-            logger.log(f"Calling on_player_entered_module of {unique_name(entity)}", LogLevel.VERBOSE)
+        for entity in list(
+            filter(lambda entity: not isinstance(entity, Player), self.module.entities)
+        ):
+            logger.log(
+                f"Calling on_player_entered_module of {unique_name(entity)}",
+                LogLevel.VERBOSE,
+            )
             entity.on_player_entered_module()
             # Player could have died in a call to on_player_entered_module so check
             if not self.alive:
                 break
 
     def print_stats(self):
-        cprint(f"You have {self.flamethrower_fuel} flamethrower fuel and {self._health} health.")
-    
+        cprint(
+            f"You have {self.flamethrower_fuel} flamethrower fuel and {self._health} health."
+        )
+
     @command("go")
     def go_in_direction(args: List[str]) -> bool:
         """go [direction] e.g. northeast/ne: Go through a door."""
@@ -204,13 +254,15 @@ class Player(Entity, Hurtable):
             cprint(f"You remember that '{direction}' is not a compass direction.")
             return False
         try:
-            player.move_to(ModuleInterface(player.module.doors[direction]), introduce=True)
+            player.move_to(
+                ModuleInterface(player.module.doors[direction]), introduce=True
+            )
             return True
         except KeyError:
             cprint("You bump into the wall.")
             player.module.print_doors()
             return False
-            
+
     # TODO: don't lock already locked
     # TODO: energy stuff
 
@@ -241,9 +293,11 @@ class Player(Entity, Hurtable):
 
 
 class Telium(Entity):
-    d_escape = Dialogue("In the corner of your eye you see an orange blob disappear through a door.",
-                        "As you enter, another door is just closing.",
-                        "You spot a flash of orange in your peripheral vision.")
+    d_escape = Dialogue(
+        "In the corner of your eye you see an orange blob disappear through a door.",
+        "As you enter, another door is just closing.",
+        "You spot a flash of orange in your peripheral vision.",
+    )
 
     ESCAPE_STEPS_RANGE = (1, 3)
 
@@ -257,7 +311,11 @@ class Telium(Entity):
             available_escapes: List[ModuleInterface] = []
             for module_id in self.module.doors.values():
                 module = ModuleInterface(module_id)
-                if module != player.previous_module and module != player.module and module != player.locked_module:
+                if (
+                    module != player.previous_module
+                    and module != player.module
+                    and module != player.locked_module
+                ):
                     available_escapes.append(module)
             if len(available_escapes) == 0:
                 break
@@ -272,15 +330,22 @@ class Telium(Entity):
 
 
 class WorkerAlien(Entity, Hurtable):
-    d_battle_start = Dialogue("{determiner} small orange alien scuttles across the floor and leaps towards you!",
-                              "{determiner} small orange alien jumps out from behind a crate!",
-                              "{determiner} small orange alien falls from the ceiling towards you!")
-    d_die = Dialogue("The alien falls lifeless to the floor, smouldering.",
-                     "The alien lies charred on the floor.",
-                     "The alien is blasted into a corner and stops moving.")
-    d_player_kill = Dialogue("The alien chews off your leg and you die.",
-                             "The alien kills you to death.")
-    d_escape = Dialogue("Wounded, the alien disappears into a vent, escaping to another module.")
+    d_battle_start = Dialogue(
+        "{determiner} small orange alien scuttles across the floor and leaps towards you!",
+        "{determiner} small orange alien jumps out from behind a crate!",
+        "{determiner} small orange alien falls from the ceiling towards you!",
+    )
+    d_die = Dialogue(
+        "The alien falls lifeless to the floor, smouldering.",
+        "The alien lies charred on the floor.",
+        "The alien is blasted into a corner and stops moving.",
+    )
+    d_player_kill = Dialogue(
+        "The alien chews off your leg and you die.", "The alien kills you to death."
+    )
+    d_escape = Dialogue(
+        "Wounded, the alien disappears into a vent, escaping to another module."
+    )
 
     def __init__(self, initial_module):
         Entity.__init__(self, initial_module)
@@ -289,9 +354,20 @@ class WorkerAlien(Entity, Hurtable):
 
     def on_player_entered_module(self):
         if self.alive:
-            cprint(self.d_battle_start.format(
-                determiner="Another" if any(not e.alive for e in filter(lambda e: isinstance(e, WorkerAlien), self.module.entities)) else "A"),
-                colour=Fore.YELLOW, character_delay=.02)
+            cprint(
+                self.d_battle_start.format(
+                    determiner="Another"
+                    if any(
+                        not e.alive
+                        for e in filter(
+                            lambda e: isinstance(e, WorkerAlien), self.module.entities
+                        )
+                    )
+                    else "A"
+                ),
+                colour=Fore.YELLOW,
+                character_delay=0.02,
+            )
             while self.alive and player.alive:
                 cprint("How much flamethrower fuel do you use against it?")
                 use_fuel = int_input(">")
@@ -310,7 +386,9 @@ class WorkerAlien(Entity, Hurtable):
                         break
                     else:
                         # If the alien is not killed yet
-                        cprint("It's not enough! The alien bites you viciously and moves to attack again.")
+                        cprint(
+                            "It's not enough! The alien bites you viciously and moves to attack again."
+                        )
                         player.hurt(self.attack)
                         if not player.alive:
                             cprint(self.d_player_kill)
@@ -325,17 +403,20 @@ class SpaceStation:
         with open(modules_filename, "r", encoding="utf-8") as file:
             for name, info in json.loads(file.read()).items():
                 self.modules[name] = Module(
-                    info["title"],
-                    info["description"],
-                    info["doors"]
+                    info["title"], info["description"], info["doors"]
                 )
-    
+
     def verify_modules_map(self):
         for module_id in self.modules:
-                module = ModuleInterface(module_id)
-                for connected_module_id in list(module.doors.values()):
-                    if not module_id in list(ModuleInterface(connected_module_id).doors.values()):
-                        logger.log(f"'{module_id}' is connected to '{connected_module_id}' but not vice versa", LogLevel.ERROR)
+            module = ModuleInterface(module_id)
+            for connected_module_id in list(module.doors.values()):
+                if not module_id in list(
+                    ModuleInterface(connected_module_id).doors.values()
+                ):
+                    logger.log(
+                        f"'{module_id}' is connected to '{connected_module_id}' but not vice versa",
+                        LogLevel.ERROR,
+                    )
 
     def deplete_energy(self, amount: int):
         """Deplete energy and check if this results in the station running out of energy."""
@@ -344,9 +425,14 @@ class SpaceStation:
             self._energy -= amount
         else:
             self._energy = 0
-        logger.log(f"{self} energy depleted by {amount}, now at {self._energy}.", LogLevel.VERBOSE)
+        logger.log(
+            f"{self} energy depleted by {amount}, now at {self._energy}.",
+            LogLevel.VERBOSE,
+        )
         if self._energy == 0:
-            cprint("The lights flicker and turn off. In their place, red emergency lights colours the station.")
+            cprint(
+                "The lights flicker and turn off. In their place, red emergency lights colours the station."
+            )
 
 
 # Globals
@@ -362,9 +448,11 @@ station.verify_modules_map()
 # Entities
 player = Player(Module.random())
 telium = Telium(Module.random([player.module]))
-worker_aliens = [WorkerAlien(Module.random([player.module])) for _ in range(NUM_WORKER_ALIENS)]
+worker_aliens = [
+    WorkerAlien(Module.random([player.module])) for _ in range(NUM_WORKER_ALIENS)
+]
 
-#list_available_commands([])
+# list_available_commands([])
 # Module.list_modules([])
 
 # Introduce module, and set visited because the player did not move there
@@ -381,7 +469,7 @@ while player.alive:
             command: Callable = available_commands[command_words[0]]
             command(command_words[1:])
         except KeyError:
-            cprint("Unrecognised command, enter \"commands\" to list them.")
+            cprint('Unrecognised command, enter "commands" to list them.')
         except IndexError:
             cprint("Not enough arguments supplied. Correct usage:")
-            cprint(command.__doc__, long_delay=.02)
+            cprint(command.__doc__, long_delay=0.02)
